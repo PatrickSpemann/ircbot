e7e7c7c0541b52f286d914f761cea3bed9acccaf
PatrickSpemann <patrick.spemann@gmail.com>
2017-09-17 14:54:12 +0200

    add admin command feature

---
+++ src/handleAdminCommand.js
@@ -0,0 +1,29 @@
+var utils = require("./ircbot-utils");
+var _client = undefined;
+
+module.exports = function (client, message) {
+    _client = client;
+    handleCommand(message);
+};
+function handleCommand(message) {
+    var command = utils.extractCommand(message);
+    var parameters = utils.extractParameters(message);
+    switch (command) {
+        case "join":
+            _client.join(parameters);
+            break;
+        case "part":
+            _client.part(parameters);
+            break;
+        case "say":
+            var firstSpace = parameters.indexOf(" ");
+            var target = parameters.substring(0, firstSpace);
+            var message = parameters.substring(firstSpace);
+            _client.say(target, message);
+            break;
+        case "nick":
+            _client.send("NICK", parameters);
+        default:
+            break;
+    }
+}
--- src/ircbot.js
+++ src/ircbot.js
@@ -2,14 +2,17 @@ var IRC = require("irc");
 var getUrls = require("get-urls");
 
 var handleCommand = require("./handleCommand");
+var handleAdminCommand = require("./handleAdminCommand");
 var imdbResolve = require("./imdbResolve");
 var youtubeResolve = require("./youtubeResolve");
 
 const networkUrl = "irc.quakenet.org";
 const nickname = "MystBot";
 const channels = ["#barakthul"];
+const adminHosts = ["Mysterion.users.quakenet.org"];
 
 var _client = undefined;
+var lastPm = undefined;
 
 module.exports.start = function () {
     _client = new IRC.Client(networkUrl, nickname, {
@@ -19,9 +22,19 @@ module.exports.start = function () {
         userName: nickname,
         realName: nickname
     });
-    _client.addListener("message", onMessage);
+    _client.addListener("pm", onPm);
+    _client.addListener("message#", onMessage);
     _client.addListener("error", onError);
 };
+function onPm(userName, message) {
+    lastPm = message;
+    _client.whois(userName, onWhoisResult);
+}
+function onWhoisResult(info) {
+    if (adminHosts.indexOf(info.host) !== -1)
+        handleAdminCommand(_client, lastPm);
+    lastPm = undefined;
+}
 function onMessage(userName, channel, message) {
     if (userName === nickname)
         return;
