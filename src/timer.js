0a57ff5963adf20a3800ab15662526f6e91ab76b
Eric E <>
2020-07-12 23:16:52 +0200

    revert timer

--- src/timer.js
+++ src/timer.js
@@ -29,7 +29,7 @@ module.exports = {
 			if (job.time < now) {
 				toBeRemoved.push(job);
 			} else {
-				schedule.scheduleJob("timer" + new Date(job.time), function (x, y) {
+				schedule.scheduleJob(new Date(job.time), function (x, y) {
 					client.say(x, y);
 				}.bind(null, job.channel, job.message));
 			}
@@ -95,7 +95,7 @@ function parseMessage(array) {
 
 function addTimer(clientInfo, _time, _message) {
 	var date = new Date(_time);
-	schedule.scheduleJob("timer" + date, function () {
+	schedule.scheduleJob(date, function () {
 		clientInfo.client.say(clientInfo.channel, _message);
 	});
 
