ec84c0abb78902c211a49fbdfea5aab58b63b3ee
PatrickSpemann <patrick.spemann@gmail.com>
2020-07-14 18:47:41 +0200

    Revert "Merge pull request #21 from Moreliaa/twitchSubs"
    This reverts commit d9c385d8f46436b4a95cc2c00a9fb89abd1689a2, reversing
    changes made to 8d25bc96b10bf8aee4f139d8cebb7e8b1260d48b.

--- src/twitchResolve.js
+++ src/twitchResolve.js
@@ -19,7 +19,6 @@ let _clientID = undefined;
 let _clientSecret = undefined;
 let _auth = undefined;
 let _callbackBaseUrl = undefined;
-let _publicIp = undefined;
 let _port = 80;
 
 let _knownLiveStreams = {};
@@ -46,8 +45,7 @@ async function initTwitchApi(clientInfo, options) {
         console.log("Twitch API - missing port.");
     _port = options.port;
 
-    let _publicIp =await publicIp.v4();
-    _callbackBaseUrl = `http://${_publicIp}`;
+    _callbackBaseUrl = `http://${await publicIp.v4()}`;
     initExpressApp();
     requestAccessToken().then(() => restoreSubscriptions()).catch((error) => console.log(error));
 }
@@ -132,7 +130,7 @@ function initExpressApp() {
         return req.twitch_hub && req.twitch_hex === req.twitch_signature;
     }
 
-    expressApp.listen(_port, _publicIp, () => console.log("Listening on port: " + _port));
+    expressApp.listen(_port, () => console.log("Listening on port: " + _port));
 }
 
 function onSubscriptionScheduleResub(userId, lease_seconds) {
